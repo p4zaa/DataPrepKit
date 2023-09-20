1588d8fdb9b00726c170b1f0acf733b0f62efa7b
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-09-20 20:22:33 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ThaiTextPrepKit',
+    version='0.1a',
+    description='Library kit for Thai language preprocessing.',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pythainlp',
+    ],
+)
