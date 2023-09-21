4b7e7ddbf0600bd26106373bfb4dbb04230e3609
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-09-21 20:07:46 +0700

    Update fix_common_words.py
    >>> แอปเปิ้ล

--- ThaiTextPrepKit/fix_common_words.py
+++ ThaiTextPrepKit/fix_common_words.py
@@ -5,6 +5,7 @@ def fix_common_word(x):
     #vowel_typo = '่้๊๋็ีัเ' # ่ ้ ๊ ๋ ็ ี ั เ
     x = re.sub('(ๅ)', '', x)
     x = re.sub(f'(แอ[พปฟผ]*[พปฟผ]*ลิเคช[ัี]*[{vowel_typo}]น)|(แอ[{vowel_typo}]*[พปฟผฯ][ฯ]*(?!เปิ[{vowel_typo}]*ล))|(aplication|application|app)', 'แอปพลิเคชัน', x)
+    x = re.sub(f'([เแ]อ[{vowel_typo}]*[ปผแบยลำพะฟห][เด้]ป[ิฺอื]*[ลน])', 'แอปเปิ้ล', x)
     x = re.sub('(scan|แสกน)', 'สแกน', x)
     x = re.sub('(time)', 'เวลา', x)
     x = re.sub(f'(ใข้วาน)|([ไใฝำ]*[ชข][{vowel_typo}]*[ง|ว|ฝ][า|ส|่]น)', 'ใช้งาน', x)
