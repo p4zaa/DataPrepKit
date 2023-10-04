72d445dacdc033e5b51b9bda02404531db1fa361
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-10-05 02:38:37 +0700

    Update pretextkit.py

--- ThaiTextPrepKit/pretextkit.py
+++ ThaiTextPrepKit/pretextkit.py
@@ -38,7 +38,7 @@ def preprocess_text(text_list, keep_stopwords=True, keep_original=True):
 
     for sentence in tqdm(text_list):
         if sentence is None:
-            preprocessed_texts.append('')
+            preprocessed_texts.append('<_>')
             continue
         else:
             sentence = str(sentence)
@@ -101,3 +101,82 @@ def preprocess_text(text_list, keep_stopwords=True, keep_original=True):
             preprocessed_texts.append(sent)
 
     return preprocessed_texts
+
+
+# For Polars >= 0.19.5 DataFrame
+def preprocess_text_polars(text_series: pl.Series, keep_stopwords=True, keep_original=True):
+
+    preprocessed_texts = []
+    url_pattern = re.compile(
+        'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    tag_pattern = re.compile(
+        '@(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    #numerical_pattern = re.compile(r'\d+(,\d+)*(\.\d+)?')  # Matches numbers with ',' and decimal part
+    thai_pattern = re.compile('[ก-๙]+')
+    english_pattern = re.compile('[A-Za-z]+')
+
+    for sentence in tqdm(text_series):
+        if sentence is None:
+            preprocessed_texts.append('<_>')
+            continue
+        else:
+            sentence = str(sentence)
+            sent = url_pattern.sub('', sentence)
+            sent = tag_pattern.sub('', sent)
+            sent = sent.replace('\\r', ' ')
+            sent = sent.replace('\\"', ' ')
+            sent = sent.replace('\\n', ' ')
+            sent = sent.replace('\n', ' ')
+
+            # You might need to provide the implementation for normalize_word function
+            sent = normalize_word(sent)
+
+            # include Thai characters in regex pattern
+            sent = re.sub('[^A-Za-z0-9ก-๙,-.%]+', ' ', sent) # Drop any character that not specify in this pattern
+
+            # Convert to lowercase before calling fix_common_word
+            sent = sent.lower().strip()
+            #sent = fix_common_words.fix_common_word(sent)
+
+            # Tokenize words
+            if keep_original:
+              # Insert spaces between English and Thai words
+              sent = re.sub(r'([A-Za-z]+)([ก-๙]+)', r'\1 \2', sent)
+              sent = re.sub(r'([ก-๙]+)([A-Za-z]+)', r'\1 \2', sent)
+
+              # Insert spaces between text and numeric values
+              sent = re.sub(r'([A-Za-z]+)([0-9]+)', r'\1 \2', sent)
+              sent = re.sub(r'([0-9]+)([A-Za-z]+)', r'\1 \2', sent)
+              sent = re.sub(r'([ก-๙]+)([0-9]+)', r'\1 \2', sent)
+              sent = re.sub(r'([0-9]+)([ก-๙]+)', r'\1 \2', sent)
+
+              sent = re.sub(r'(%)([ก-๙]+|[A-Za-z]+|[0-9]+)', r'\1 \2', sent)
+
+              tokens = word_tokenize(sent, keep_whitespace=True)
+
+            else:
+              tokens = word_tokenize(sent, keep_whitespace=False)
+
+            filtered_tokens = []
+            for token in tokens:
+              match keep_stopwords:
+                case False:
+                  if token not in stopwords and len(token) > 1 and not keep_original:
+                    filtered_tokens.append(token)
+                  elif token not in stopwords and keep_original:
+                    filtered_tokens.append(token)
+                case True:
+                  if len(token) > 1 and not keep_original:
+                    filtered_tokens.append(token)
+                  else:
+                    filtered_tokens.append(token)
+
+            # tokenize and remove stopwords
+            if keep_original:
+              sent = ''.join(e for e in filtered_tokens)
+            else:
+              sent = ' '.join(e for e in filtered_tokens)
+
+            preprocessed_texts.append(sent)
+
+    return pl.Series(preprocessed_texts, dtype=pl.Utf8)
