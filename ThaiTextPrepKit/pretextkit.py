99fc250ab52eb27fa2e1c48917fab0eaa2fe9797
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-11-29 20:19:18 +0700

    Update pretextkit.py
    Update on preprocess_text_polars function

--- ThaiTextPrepKit/pretextkit.py
+++ ThaiTextPrepKit/pretextkit.py
@@ -110,7 +110,7 @@ def preprocess_text(text_list, keep_stopwords=True, keep_original=True):
 
     return preprocessed_texts
 
-
+'''
 # For Polars >= 0.19.5 DataFrame
 def preprocess_text_polars(text_series: pl.Series, custom_dict=None, keep_stopwords: bool=True, keep_original: bool=None, return_token_list: bool=None):
     
@@ -207,3 +207,88 @@ def preprocess_text_polars(text_series: pl.Series, custom_dict=None, keep_stopwo
               preprocessed_texts.append(sent)
 
     return pl.Series(preprocessed_texts) if return_token_list else pl.Series(preprocessed_texts, dtype=pl.Utf8)
+'''
+
+# For Polars >= 0.19.0 DataFrame
+def preprocess_text_polars(series: pl.Series, custom_dict=None, keep_stopwords: bool=True, keep_original: bool=True, return_token_list: bool=False):
+
+    if not isinstance(keep_original, (bool)) or not isinstance(return_token_list, (bool)):
+      raise ValueError("'keep_original' and 'return_token_list' only execpt boolean.")
+    elif keep_original and return_token_list:
+      raise ValueError("Only one of 'keep_original' and 'return_token_list' can be passed at a time.")
+
+    trie = dict_trie(dict_source=custom_dict) if custom_dict else None
+    
+    def preprocess(text, trie=trie):
+      url_pattern = re.compile(
+          'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+      tag_pattern = re.compile(
+          '@(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+      thai_pattern = re.compile('[ก-๙]+')
+      english_pattern = re.compile('[A-Za-z]+')
+
+      text = str(text)
+      sent = url_pattern.sub('', text)
+      sent = tag_pattern.sub('', sent)
+      sent = sent.replace('\\r', ' ')
+      sent = sent.replace('\\"', ' ')
+      sent = sent.replace('\\n', ' ')
+      sent = sent.replace('\n', ' ')
+
+      sent = normalize_word(sent)
+
+      # include Thai characters in regex pattern
+      sent = re.sub('[^A-Za-z0-9ก-๙,-.%]+', ' ', sent) # Drop any character that not specify in this pattern
+
+      # Convert to lowercase before calling fix_common_word
+      sent = sent.lower().strip()
+      sent = fix_common_words.fix_common_word(sent)
+
+      # Tokenize words
+      if keep_original:
+        # Insert spaces between English and Thai words
+        sent = re.sub(r'([A-Za-z]+)([ก-๙]+)', r'\1 \2', sent)
+        sent = re.sub(r'([ก-๙]+)([A-Za-z]+)', r'\1 \2', sent)
+
+        # Insert spaces between text and numeric values
+        sent = re.sub(r'([A-Za-z]+)([0-9]+)', r'\1 \2', sent)
+        sent = re.sub(r'([0-9]+)([A-Za-z]+)', r'\1 \2', sent)
+        sent = re.sub(r'([ก-๙]+)([0-9]+)', r'\1 \2', sent)
+        sent = re.sub(r'([0-9]+)([ก-๙]+)', r'\1 \2', sent)
+
+        sent = re.sub(r'(%)([ก-๙]+|[A-Za-z]+|[0-9]+)', r'\1 \2', sent)
+
+        tokens = word_tokenize(sent, keep_whitespace=True, join_broken_num=True, custom_dict=trie)
+
+      else:
+        tokens = word_tokenize(sent, keep_whitespace=False, join_broken_num=True, custom_dict=trie)
+
+      filtered_tokens = []
+      for token in tokens:
+        match keep_stopwords:
+          case False:
+            if token not in stopwords and len(token) > 1 and not keep_original:
+              filtered_tokens.append(token)
+            elif token not in stopwords and keep_original:
+              filtered_tokens.append(token)
+          case True:
+            if len(token) > 1 and not keep_original:
+              filtered_tokens.append(token)
+            else:
+              filtered_tokens.append(token)
+
+      # tokenize and remove stopwords
+      match keep_original:
+        case True:
+          sent = ''.join(e for e in filtered_tokens)
+        case False:
+          sent = ' '.join(e for e in filtered_tokens)
+        case _:
+          raise ValueError("'keep_original' only execpt none type or boolean.")
+
+      if return_token_list:
+        return filtered_tokens
+      else:
+        return sent
+
+    return series.map_elements(preprocess)
